8271dc3f8b71d651564197e14c855c4eb57a2c2f
Ledjo <ledjo.lleshaj@gmail.com>
2022-05-06 07:47:23 -0700

    Added implementation of useful functions for semaphores

--- semaphore.c
+++ semaphore.c
@@ -2,5 +2,97 @@
 /// @brief Contiene l'implementazione delle funzioni
 ///         specifiche per la gestione dei SEMAFORI.
 
+#include <sys/stat.h>
+#include <sys/sem.h>
+#include <errno.h>
+
 #include "err_exit.h"
 #include "semaphore.h"
+
+
+int createSemaphores(key_t key, int n_sem) {
+    int semid = semget(key, n_sem, IPC_CREAT | S_IRUSR | S_IWUSR);
+
+    if (semid == -1)
+        ErrExit("semget failed");
+    return semid;
+}
+
+int getSemaphores(key_t key, int n_sem) {
+    int semid = semget(key, n_sem, S_IRUSR | S_IWUSR);
+
+    if (semid == -1)
+        ErrExit("semget failed");
+    return semid;
+}
+
+
+void semOp(int semid, unsigned short sem_num, short sem_op) {
+    struct sembuf sop = {.sem_num = sem_num, .sem_op = sem_op, .sem_flg = 0};
+
+    if (semop(semid, &sop, 1) == -1)
+        ErrExit("semop failed");
+}
+
+int semOpNoBlocc(int semid, unsigned short sem_num, short sem_op) {
+    struct sembuf sop = {.sem_num = sem_num, .sem_op = sem_op, .sem_flg = IPC_NOWAIT};
+    if (semop(semid, &sop, 1) == -1){
+        if (errno == EAGAIN){
+            return -1;
+        }
+        else{
+            ErrExit("semop failed");
+        }
+    }
+
+    return 0;
+}
+
+void semWaitZero(int semid, int sem_num) {
+    semOp(semid, sem_num, 0);
+}
+
+void semWait(int semid, int sem_num) {
+    semOp(semid, sem_num, -1);
+}
+
+int semWaitNoBlocc(int semid, int sem_num) {
+    return semOpNoBlocc(semid, sem_num, -1);
+}
+
+void semSignal(int semid, int sem_num) {
+    semOp(semid, sem_num, 1);
+}
+
+
+void semSetVal(int semid, int sem_num, int val) {
+    union semun arg;
+    arg.val = val;
+
+    if (semctl(semid, sem_num, SETVAL, arg) == -1) {
+        ErrExit("semctl SETVAL");
+    }
+}
+
+
+void semSetAll(int semid, short unsigned int values[]) {
+    union semun arg;
+    arg.array = values;
+
+    // Inizializza il set di semafori
+    if (semctl(semid, 0/*semnum: ignored*/, SETALL, arg) == -1)
+        ErrExit("semctl SETALL");
+}
+
+
+void semDelete(int semid) {
+    if (semctl(semid, 0/*semnum: ignored*/, IPC_RMID, 0/*arg: ignored*/) == -1) {
+        ErrExit("semctl failed");
+    }
+}
+
+
+void semSetPerm(int semid, struct semid_ds arg) {
+    if (semctl(semid, 0 /*semnum: ignored*/, IPC_SET, arg) == -1)
+        ErrExit("semctl IPC_SET failed");
+}
--- semaphore.h
+++ semaphore.h
@@ -3,3 +3,129 @@
 ///         specifiche per la gestione dei SEMAFORI.
 
 #pragma once
+
+/**
+ * Union usata dalla system call semctl().
+*/
+union semun {
+    /// usato se si lavora su un singolo semaforo.
+    /// Usato dalla operazione SETVAL
+    int val;
+
+    /// usato per lavorare sullo stato globale del semaforo.
+    /// Usato dalle operazioni IPC_STAT e IPC_SET
+    struct semid_ds * buf;
+
+    /// per eseguire operazioni su tutti i semafori.
+    /// Usato dalle operazioni GETALL e SETALL
+    unsigned short * array;
+};
+
+
+/**
+ * @brief Ottiene un insieme di semafori gia' creato
+ *
+ * @param key Chiave IPC
+ * @param n_sem Numero semafori da ottenere/creare
+*/
+int getSemaphores(key_t key, int n_sem);
+
+/**
+ * @brief Crea un insieme di semafori
+ *
+ * @param key Chiave IPC
+ * @param n_sem Numero semafori da ottenere/creare
+*/
+int createSemaphores(key_t key, int n_sem);
+
+
+/**
+ * @brief Funzione di supporto per manipolare i valori di un set di semafori.
+ *
+ * @param semid Identificatore del set di semafori
+ * @param sem_num Indice di un semaforo nel set
+ * @param sem_op Operazione eseguita sul semaforo sem_num
+*/
+void semOp(int semid, unsigned short sem_num, short sem_op);
+
+/**
+ * Funzione di supporto per manipolare i valori di un set di semafori in modo non bloccante.
+ * Restituisce -1 se il semaforo ha tentato di bloccare il processo, 0 altrimenti.
+ *
+ * @param semid Identificatore del set di semafori
+ * @param sem_num Indice di un semaforo nel set
+ * @param sem_op Operazione eseguita sul semaforo sem_num
+ * @return -1 se il semaforo ha tentato di bloccare il processo, 0 altrimenti
+*/
+int semOpNoBlocc(int semid, unsigned short sem_num, short sem_op);
+
+/**
+ * Attende che il semaforo sem_num raggiunga il valore zero.
+ * > Prima di usarla per sincronizzare processi richiamare semWait sullo stesso semaforo.
+ *
+ * @param semid Identificatore del set di semafori
+ * @param sem_num Indice di un semaforo nel set
+*/
+void semWaitZero(int semid, int sem_num);
+
+
+/**
+ * @brief Esegue la wait sul semaforo sem_num: decrementa il valore di 1 ed eventualmente mette in attesa il processo.
+ *
+ * @param semid Identificatore del set di semafori
+ * @param sem_num Indice di un semaforo nel set
+*/
+void semWait(int semid, int sem_num);
+
+/**
+ * Esegue la wait non bloccante sul semaforo sem_num: tenta di decrementare il suo valore di 1.
+ * Restituisce -1 se il semaforo ha tentato di bloccare il processo, 0 altrimenti.
+ *
+ * @param semid Identificatore del set di semafori
+ * @param sem_num Indice di un semaforo nel set
+ * @return -1 se il semaforo ha tentato di bloccare il processo, 0 altrimenti
+*/
+int semWaitNoBlocc(int semid, int sem_num);
+
+/**
+ * @brief Esegue la signal sul semaforo sem_num: incrementa il valore di 1.
+ *
+ * @param semid Identificatore del set di semafori
+ * @param sem_num Indice di un semaforo nel set
+*/
+void semSignal(int semid, int sem_num);
+
+/**
+ * @brief Inizializza il valore del semaforo sem_num al valore val.
+ *
+ * @param semid Identificatore del set di semafori
+ * @param sem_num Indice di un semaforo nel set
+ * @param val Valore a cui impostare il semaforo sem_num
+*/
+void semSetVal(int semid, int sem_num, int val);
+
+
+/**
+ * @brief Inizializza i valori del set di semafori semid ai valori in values.
+ *
+ * @param semid Identificatore del set di semafori
+ * @param values Array di valori a cui impostare ogni semaforo del set
+*/
+void semSetAll(int semid, short unsigned int values[]);
+
+
+/**
+ * @brief Cancella il set di semafori svegliando eventuali processi in attesa.
+ *
+ * @param semid Identificatore del set di semafori
+ */
+void semDelete(int semid);
+
+
+/**
+ * @brief Imposta i permessi su un set di semafori.
+ *
+ * @param semid Identificatore del set di semafori
+ * @param arg Contiene i permessi da impostare
+*/
+void semSetPerm(int semid, struct semid_ds arg);
